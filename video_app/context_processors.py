d17011c85e84b79acaf89a3d12a76ec922ea4aba
NishantGhanate <nishant7.ng@gmail.com>
2023-03-23 09:43:42 +0530

    ngix bug fix - added volume ref

--- video_app/context_processors.py
+++ video_app/context_processors.py
@@ -4,4 +4,8 @@ def get_google_form(request):
     return {
         'get_google_form' :  settings.GOOGLE_FORM_URL
     }
-    
+
+def get_whatsapp_link(request):
+    return {
+        'get_whatsapp_link': settings.WHATS_APP_LINK
+    }
